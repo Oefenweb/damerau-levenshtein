dba39d33aac1ec98175f43bdca85e1423dc5c0aa
Mischa ter Smitten <mischa@tersmitten.nl>
2014-08-21 12:05:28 +0200

    Added tests for getSimilarity

--- test/DamerauLevenshteinTest.php
+++ test/DamerauLevenshteinTest.php
@@ -9,5 +9,51 @@ class DamerauLevenshteinTest extends PHPUnit_Framework_TestCase {
  * @return void
  */
 	public function testGetSimilarity() {
+		$inputs = array(
+			array('foo', 'foo'),
+			array('foo', 'fooo'),
+			array('foo', 'bar'),
+
+			array('123', '12'),
+			array('qwe', 'qwa'),
+			array('awe', 'qwe'),
+			array('фыв', 'фыа'),
+			array('vvvqw', 'vvvwq'),
+			array('qw', 'wq'),
+			array('qq', 'ww'),
+			array('qw', 'qw'),
+			array('пионер', 'плеер'),
+			array('пионер', 'пионеер'),
+			array('пионер', 'поинер'),
+			array('pioner', 'poner'),
+			array('пионер', 'понер'),
+		);
+		$outputs = array(
+			0,
+			1,
+			3,
+
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			2,
+			0,
+			3,
+			1,
+			1,
+			1,
+			1,
+		);
+
+		foreach ($inputs as $i => $input) {
+			$DamerauLevenshtein = new DamerauLevenshtein($input[0], $input[1]);
+			$result = $DamerauLevenshtein->getSimilarity();
+			$expected = $outputs[$i];
+
+			$this->assertEquals($expected, $result);
+		}
 	}
 }
