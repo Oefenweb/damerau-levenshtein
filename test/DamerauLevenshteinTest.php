724c343ecab9fcd361ac887dd010df68b7714ddb
Mischa ter Smitten <mischa@tersmitten.nl>
2014-08-21 10:28:43 +0200

    Added (missing) doc comment

--- test/DamerauLevenshteinTest.php
+++ test/DamerauLevenshteinTest.php
@@ -3,6 +3,11 @@ require sprintf('%s/../src/DamerauLevenshtein.php', dirname(__FILE__));
 
 class DamerauLevenshteinTest extends PHPUnit_Framework_TestCase {
 
+/**
+ * Test for getSimilarity.
+ *
+ * @return void
+ */
 	public function testGetSimilarity() {
 	}
 }
