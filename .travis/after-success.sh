b3df8dbf059a98e73b6a0bb8ec79acb67ef2df88
Mischa ter Smitten <mtersmitten@oefenweb.nl>
2015-06-05 16:59:31 +0200

    Readded .coveralls.yml file

--- .travis/after-success.sh
+++ .travis/after-success.sh
@@ -8,7 +8,5 @@ thisFile="$(readlink -f ${0})";
 thisFilePath="$(dirname ${thisFile})";
 #
 if [ "${COVERALLS}" = '1' ]; then
-  ls -lha .;
-  cat .coveralls.yml;
   ~/.composer/vendor/bin/coveralls -c .coveralls.yml -v;
 fi
--- .travis/before-script.sh
+++ .travis/before-script.sh
@@ -19,9 +19,5 @@ else
   composer global require --dev 'phpunit/phpunit=4.*';
   if [ "${COVERALLS}" = '1' ]; then
     composer global require --dev 'satooshi/php-coveralls:dev-master';
-
-    printf '# for php-coveralls\nsrc_dir: src\ncoverage_clover: build/logs/clover.xml\njson_path: build/logs/coveralls-upload.json\n' > .coveralls.yml;
-		ls -lha .;
-		cat .coveralls.yml;
   fi
 fi
