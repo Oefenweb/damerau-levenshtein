e9449b2ae36a1919af614edf0bab68613df114d1
Mischa ter Smitten <mtersmitten@oefenweb.nl>
2015-06-05 13:34:24 +0200

    Improved .travis.yml

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,5 +7,4 @@ set -o pipefail;
 thisFile="$(readlink -f ${0})";
 thisFilePath="$(dirname ${thisFile})";
 #
-
 composer install --no-ansi --no-progress --no-interaction --prefer-source;
--- .travis/script.sh
+++ .travis/script.sh
@@ -8,10 +8,11 @@ thisFile="$(readlink -f ${0})";
 thisFilePath="$(dirname ${thisFile})";
 #
 if [ "${PHPCS}" = '1' ]; then
-	vendor/bin/phpcs --config-set installed_paths vendor/cakephp/cakephp-codesniffer;
-	vendor/bin/phpcs --standard=CakePHP -n src;
+  vendor/bin/phpcs --config-set \
+    installed_paths "vendor/cakephp/cakephp-codesniffer,vendor/oefenweb/cakephp-codesniffer";
+  vendor/bin/phpcs --standard=Oefenweb src tests;
 elif [ "${COVERALLS}" = 1 ]; then
-	vendor/bin/phpunit --stderr --configuration phpunit.xml --coverage-clover build/logs/clover.xml;
+  vendor/bin/phpunit --stderr --configuration phpunit.xml --coverage-clover build/logs/clover.xml;
 else
-	vendor/bin/phpunit --stderr --configuration phpunit.xml;
+  vendor/bin/phpunit --stderr --configuration phpunit.xml;
 fi
