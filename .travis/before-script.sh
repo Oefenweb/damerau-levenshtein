26b917193dc3017c3917ed7ad3300bb2be10a326
Mischa ter Smitten <mtersmitten@oefenweb.nl>
2015-06-05 14:58:02 +0200

    Improved .travis.yml

--- .travis/before-script.sh
+++ .travis/before-script.sh
@@ -18,5 +18,7 @@ else
   composer global require --dev 'phpunit/phpunit=4.*';
   if [ "${COVERALLS}" = '1' ]; then
     composer global require --dev 'satooshi/php-coveralls:dev-master';
+
+    printf "src_dir: src\ncoverage_clover: build/logs/clover.xml\njson_path: build/logs/coveralls-upload.json" > .coveralls.yml
   fi
 fi
