7109dfbf7d8ab3fd0a2eda9a7b6a4d4fd37b6443
Mischa ter Smitten <mischa@tersmitten.nl>
2019-01-22 23:28:39 +0100

    Use PHPUnit 6 for PHP 7.0

--- .travis/before-script.sh
+++ .travis/before-script.sh
@@ -13,5 +13,10 @@ composer install --no-ansi --no-progress --no-interaction --prefer-source;
 if [ "${PHPCS}" = '1' ]; then
   composer require --dev 'squizlabs/php_codesniffer=2.*';
 else
-  composer require --dev 'phpunit/phpunit=^7';
+  PHP_VERSION="$(php -r 'echo phpversion();')";
+  if [ "${PHP_VERSION:0:3}" == '7.0' ]; then
+    composer require --dev 'phpunit/phpunit=^6';
+  else
+    composer require --dev 'phpunit/phpunit=^7';
+  fi
 fi
